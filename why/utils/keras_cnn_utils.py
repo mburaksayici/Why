11beb11ebd7934c7a4b600f29917f3d8bac9b24e
mburaksayici <mburaksayici@gmail.com>
2022-01-13 05:43:33 +0300

    feat(utils): added keras utils

--- why/utils/keras_cnn_utils.py
+++ why/utils/keras_cnn_utils.py
@@ -3,7 +3,6 @@
 import tensorflow as tf
 
 
-
 def get_layers_type(model):
     """
     Returns model layer types dict of given Keras Model.
@@ -13,20 +12,25 @@ def get_layers_type(model):
         model_layers[idx] = {"layer_name": layer.name,"is_cnn":True if isinstance(layer, tf.keras.layers.Conv2D) else False }
     return model_layers
 
-def separate_model(model,layer_index):
+def separate_model(model,layer_index=None):
     """
     Separates model into explaining, and post-explain models.
     """
     model_layers = get_layers_type(model)
+    
+    # Use last CNN if index not given.
+    if not layer_index:
+        layer_index = [i for i in list(reversed(list(model_layers.keys()))) if model_layers[i]["is_cnn"]==True][0]
 
-    explaining_conv_layer = model.get_layer(layer_index)
+    explaining_conv_layer = model.get_layer(model_layers[layer_index]["layer_name"])
     explaining_conv_layer_model = tf.keras.Model(model.inputs, explaining_conv_layer.output)
 
     post_explain_input = tf.keras.Input(shape=explaining_conv_layer_model.output.shape[1:])
 
     x = post_explain_input
-    for layer_name in [v["layer_name"] for k,v in model_layers.items() if k > layer_index]:
+    for layer_name in model.layers[layer_index:]:
         x = model.get_layer(layer_name)(x)
+
     post_explain_model = tf.keras.Model(post_explain_input, x)
 
     return explaining_conv_layer_model, post_explain_model
