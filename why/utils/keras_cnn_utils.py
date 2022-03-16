2d32f32781d8b24c8e2c0827d7801646401f473f
mburaksayici <mburaksayici@gmail.com>
2022-03-16 11:15:08 +0300

    fix(explain): minor fixes on keras gradcam

--- why/utils/keras_cnn_utils.py
+++ why/utils/keras_cnn_utils.py
@@ -9,27 +9,39 @@ def get_layers_type(model):
     """
     model_layers = {}
     for idx, layer in enumerate(model.layers):
-        model_layers[idx] = {"layer_name": layer.name,"is_cnn":True if isinstance(layer, tf.keras.layers.Conv2D) else False }
+        model_layers[idx] = {
+            "layer_name": layer.name,
+            "is_cnn": True if isinstance(layer, tf.keras.layers.Conv2D) else False,
+        }
     return model_layers
 
-def separate_model(model,layer_index=None):
+
+def separate_model(model, layer_index=None):
     """
     Separates model into explaining, and post-explain models.
     """
     model_layers = get_layers_type(model)
-    
+
     # Use last CNN if index not given.
     if not layer_index:
-        layer_index = [i for i in list(reversed(list(model_layers.keys()))) if model_layers[i]["is_cnn"]==True][0]
+        layer_index = [
+            i
+            for i in list(reversed(list(model_layers.keys())))
+            if model_layers[i]["is_cnn"] == True
+        ][0]
 
     explaining_conv_layer = model.get_layer(model_layers[layer_index]["layer_name"])
-    explaining_conv_layer_model = tf.keras.Model(model.inputs, explaining_conv_layer.output)
+    explaining_conv_layer_model = tf.keras.Model(
+        model.inputs, explaining_conv_layer.output
+    )
 
-    post_explain_input = tf.keras.Input(shape=explaining_conv_layer_model.output.shape[1:])
+    post_explain_input = tf.keras.Input(
+        shape=explaining_conv_layer_model.output.shape[1:]
+    )
 
     x = post_explain_input
-    for layer_name in model.layers[layer_index:]:
-        x = model.get_layer(layer_name)(x)
+    for layer_idx in list(model_layers.keys())[layer_index:]:
+        x = model.get_layer(model_layers[layer_idx]["layer_name"])(x)
 
     post_explain_model = tf.keras.Model(post_explain_input, x)
 
